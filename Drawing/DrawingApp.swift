d8c2fdba4b817068618f5557fdff474eb91255de
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-25 12:57:12 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Sebastien REMY on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
