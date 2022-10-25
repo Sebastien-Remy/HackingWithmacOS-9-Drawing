39e02a4b5cf4e80237f7105443eed4a693ff4f4c
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-25 17:18:29 +0200

    Transforming shapes using CGAffineTransform and even-odd fills

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
+        
         VStack {
-            Text("Triangle")
+//            Text("Triangle")
 //            Path { path in
 //                path.move(to: CGPoint(x: 200, y: 100))
 //                path.addLine(to: CGPoint(x: 100, y: 300))
@@ -35,12 +41,52 @@ struct ContentView: View {
 //            Circle()
 //                .strokeBorder(.blue, lineWidth: 40)
 //                .frame(width: 500, height: 500)
+//
+//            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true)
+//                .strokeBorder(.blue, lineWidth: 40)
+//                .frame(width: 500, height: 500)
             
-            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true)
-                .strokeBorder(.blue, lineWidth: 40)
-                .frame(width: 500, height: 500)
+            Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
+                .fill(.red, style: FillStyle(eoFill: true))
+                .frame(width: 300)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
         }
         .padding()
+    }
+}
+
+struct Flower: Shape {
+    var petalOffSet: Double = -20 // How much to move patal away from center
+    var petalWidth: Double = 100 // How wide to make each petal
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // Count from 0 to pi * 2, moving up pi / 8
+        
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffSet, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/position transformation top the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // Add it to the main path
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
     }
 }
 
