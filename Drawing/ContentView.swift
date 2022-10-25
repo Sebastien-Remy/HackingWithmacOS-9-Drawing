0cfc5c857ee7193efcfa7ef24ed92a1818df7a1c
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-25 17:57:17 +0200

    High performance Metal,

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -13,65 +13,98 @@ struct ContentView: View {
 //    @State private var petalOffset = -20.0
 //    @State private var petalWidth = 100.0
     
+    @State private var colorCycle = 0.0
     var body: some View {
-        
-        VStack {
-//            Text("Triangle")
-//            Path { path in
-//                path.move(to: CGPoint(x: 200, y: 100))
-//                path.addLine(to: CGPoint(x: 100, y: 300))
-//                path.addLine(to: CGPoint(x: 300, y: 300))
-//                path.addLine(to: CGPoint(x: 200, y: 100))
-////                path.closeSubpath()
-//            }
-////            .fill(.blue)
-////            .stroke(.red, lineWidth: 10)
-            ///
-            /// Shape
-//            Triangle()
-//                .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round,lineJoin: .round))
-//                .frame(width: 300, height: 300)
-//                .padding(50)
-            
-//            // Arc
-//            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true) .stroke(.blue, lineWidth: 10)
-//                .frame(width: 300, height: 300)
-//                .padding(50)
-//
-//            Circle()
-//                .strokeBorder(.blue, lineWidth: 40)
-//                .frame(width: 500, height: 500)
-//
-//            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true)
-//                .strokeBorder(.blue, lineWidth: 40)
-//                .frame(width: 500, height: 500)
-//
-//            Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
-//                .fill(.red, style: FillStyle(eoFill: true))
-//                .frame(width: 300)
-//
-//            Text("Offset")
-//            Slider(value: $petalOffset, in: -40...40)
-//                .padding([.horizontal, .bottom])
-//            Text("Width")
-//            Slider(value: $petalWidth, in: 0...100)
-//                .padding(.horizontal)
-            
-            
-            
-            
-            Text("Hello world")
-                .frame(width: 300, height: 100)
-                .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
-            Text("Hello")
-                .frame(width: 300, height: 100).border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5)), width: 30)
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.2), lineWidth: 20)
-                .frame(width: 300, height: 100)
-            
+            VStack {
+                //            Text("Triangle")
+                //            Path { path in
+                //                path.move(to: CGPoint(x: 200, y: 100))
+                //                path.addLine(to: CGPoint(x: 100, y: 300))
+                //                path.addLine(to: CGPoint(x: 300, y: 300))
+                //                path.addLine(to: CGPoint(x: 200, y: 100))
+                ////                path.closeSubpath()
+                //            }
+                ////            .fill(.blue)
+                ////            .stroke(.red, lineWidth: 10)
+                ///
+                /// Shape
+                //            Triangle()
+                //                .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round,lineJoin: .round))
+                //                .frame(width: 300, height: 300)
+                //                .padding(50)
                 
+                //            // Arc
+                //            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true) .stroke(.blue, lineWidth: 10)
+                //                .frame(width: 300, height: 300)
+                //                .padding(50)
+                //
+                //            Circle()
+                //                .strokeBorder(.blue, lineWidth: 40)
+                //                .frame(width: 500, height: 500)
+                //
+                //            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true)
+                //                .strokeBorder(.blue, lineWidth: 40)
+                //                .frame(width: 500, height: 500)
+                //
+                //            Flower(petalOffSet: petalOffset, petalWidth: petalWidth)
+                //                .fill(.red, style: FillStyle(eoFill: true))
+                //                .frame(width: 300)
+                //
+                //            Text("Offset")
+                //            Slider(value: $petalOffset, in: -40...40)
+                //                .padding([.horizontal, .bottom])
+                //            Text("Width")
+                //            Slider(value: $petalWidth, in: 0...100)
+                //                .padding(.horizontal)
+                
+                
+                //            // Using imagePaint
+                //
+                //            Text("Hello world")
+                //                .frame(width: 300, height: 100)
+                //                .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
+                //            Text("Hello")
+                //                .frame(width: 300, height: 100).border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5)), width: 30)
+                //            Capsule()
+                //                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.2), lineWidth: 20)
+                //                .frame(width: 300, height: 100)
+                
+                
+                //            }
+                ColorCyclingCircle(amount: colorCycle)
+                    .frame(width: 300, height: 300)
+                Slider(value: $colorCycle)
+                    .padding()
+            }
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps, id: \.self) { value in
+                Circle()
+                    .inset(by: Double(value))
+//                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(
+                        LinearGradient (
+                            gradient: Gradient(colors: [color(for: value, brightness: 1), color(for: value, brightness: 0.4)]),
+                                        startPoint: .top,
+                                        endPoint: .bottom),
+                        lineWidth: 2
+                        )
+            }
         }
-        .padding()
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        if targetHue > 1 { targetHue -= 1 }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
