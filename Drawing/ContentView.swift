c6470bf7c3402b75c43d6304049d937fc81c3135
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-25 16:46:37 +0200

    Paths vs Shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -11,22 +11,68 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Triangle")
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 100))
-                path.addLine(to: CGPoint(x: 100, y: 300))
-                path.addLine(to: CGPoint(x: 300, y: 300))
-                path.addLine(to: CGPoint(x: 200, y: 100))
-//                path.closeSubpath()
-            }
-//            .fill(.blue)
-//            .stroke(.red, lineWidth: 10)
-            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round,lineJoin: .round))
-            .frame(width: 500, height: 500)
-            .padding(50)
+//            Path { path in
+//                path.move(to: CGPoint(x: 200, y: 100))
+//                path.addLine(to: CGPoint(x: 100, y: 300))
+//                path.addLine(to: CGPoint(x: 300, y: 300))
+//                path.addLine(to: CGPoint(x: 200, y: 100))
+////                path.closeSubpath()
+//            }
+////            .fill(.blue)
+////            .stroke(.red, lineWidth: 10)
+            ///
+            /// Shape
+//            Triangle()
+//                .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round,lineJoin: .round))
+//                .frame(width: 300, height: 300)
+//                .padding(50)
+            Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true) .stroke(.blue, lineWidth: 10)
+                .frame(width: 300, height: 300)
+                .padding(50)
+            
+            
         }
         .padding()
     }
 }
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+              
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxX))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+    
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2,
+                    startAngle: modifiedStart,
+                    endAngle: modifiedEnd,
+                    clockwise: clockwise)
+        
+        return path
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
